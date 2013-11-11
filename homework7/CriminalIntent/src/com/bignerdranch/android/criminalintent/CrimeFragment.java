7dde29f06154be76a963f5856b401fc2b5c6a0a2
WDElmore <wdelmore0@gmail.com>
2013-11-10 23:17:16 -0500

    chapter 12 complete

--- homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,9 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -24,12 +28,19 @@ public class CrimeFragment extends Fragment {
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
 	
+	private static final String DIALOG_DATE = "date";
+	private static final int REQUEST_DATE = 0;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
 		
 		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+	}
+	
+	public void updateDate() {
+		mDateButton.setText(mCrime.getDate().toString());
 	}
 
 	@Override
@@ -52,9 +63,16 @@ public class CrimeFragment extends Fragment {
 			}
 		});
 		
-		mDateButton= (Button)v.findViewById(R.id.crime_date);
-		mDateButton.setText(mCrime.getDate().toString());
-		mDateButton.setEnabled(false);
+		mDateButton = (Button)v.findViewById(R.id.crime_date);
+		updateDate();
+		mDateButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				FragmentManager fm = getActivity().getSupportFragmentManager();
+				DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+				dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+				dialog.show(fm, DIALOG_DATE);
+			}
+		});
 		
 		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
 		mSolvedCheckBox.setChecked(mCrime.isSolved());
@@ -66,6 +84,16 @@ public class CrimeFragment extends Fragment {
 		});
 		
 		return v;
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode != Activity.RESULT_OK) return;
+		if (requestCode == REQUEST_DATE) {
+			Date date = (Date)data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+			mCrime.setDate(date);
+			updateDate();
+		}
 	}
 	
 	public static CrimeFragment newInstance(UUID crimeId) {
