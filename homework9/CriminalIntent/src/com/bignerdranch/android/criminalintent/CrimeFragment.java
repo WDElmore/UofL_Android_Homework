3d6bdc77a0a358b4d8154032c49ca6547e62c9cb
WDElmore <wdelmore0@gmail.com>
2013-11-25 01:10:47 -0500

    CH19 complete

--- homework9/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ homework9/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -22,6 +23,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class CrimeFragment extends Fragment {
 	public static final String EXTRA_CRIME_ID =
@@ -31,6 +33,7 @@ public class CrimeFragment extends Fragment {
 	private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
+	private ImageButton mPhotoButton;
 	
 	private static final String DIALOG_DATE = "date";
 	private static final int REQUEST_DATE = 0;
@@ -93,8 +96,25 @@ public class CrimeFragment extends Fragment {
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 				// Set the crime's solved property
 				mCrime.setSolved(isChecked);
+			}			
+		});
+		
+		mPhotoButton = (ImageButton)v.findViewById(R.id.crime_imageButton);
+		mPhotoButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getActivity(), CrimeCameraActivity.class);
+				startActivity(i);
 			}
 		});
+		
+		// If camera is not available, disable camera functionality
+		PackageManager pm = getActivity().getPackageManager();
+		if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA) &&
+				!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
+			mPhotoButton.setEnabled(false);
+			
+		}
 		
 		return v;
 	}
